0417e6359d3b13276ddd115dbccc4d7d4bcca4cb
Bernard Xiong <bernard.xiong@gmail.com>
2017-05-03 00:26:27 +0800

    Update example.c

--- example.c
+++ example.c
@@ -3,7 +3,7 @@
 
 int example(int argc, char** argv)
 {
-	rt_kprintf(¡±Hi, this is an example package for rt-thread!\n");
-	return 0;
+    rt_kprintf("Hi, this is an example package for rt-thread!\n");
+    return 0;
 }
 MSH_CMD_EXPORT(example, example command);
